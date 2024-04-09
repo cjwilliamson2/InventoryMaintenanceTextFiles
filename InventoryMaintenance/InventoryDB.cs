4da0229f3620f1e010a80e5b2380e7d1d734a37b
cjwilliamson2 <cjwilliamson2@pstcc.edu>
2024-04-09 10:54:28 -0400

    Implemented GetItems() in InventoryDB class

--- InventoryMaintenance/InventoryDB.cs
+++ InventoryMaintenance/InventoryDB.cs
@@ -9,7 +9,42 @@ namespace InventoryMaintenance
             // create the list
             List<InventoryItem> items = new();
 
-            // Add code here to read the text file into the List<InventoryItem> object.
+            //TODO: Add code here to read the text file into the List<InventoryItem> object.
+
+            // Create a FileStream to open our file for reading
+            // Create a Streamreder to read from file
+
+            try
+            {
+                FileStream inFile = new FileStream(Path, FileMode.OpenOrCreate, FileAccess.Read);
+                using StreamReader textIn = new StreamReader(inFile);
+
+                while (textIn.Peek() != -1)
+                {
+                    try
+                    {
+                        string line = textIn.ReadLine()!;
+                        string[] itemProperties = line.Split("|");
+
+                        //Create an InventoryItem
+
+                        InventoryItem item = new InventoryItem(
+                            Convert.ToInt32(itemProperties[0]),
+                            itemProperties[1],
+                            Convert.ToDecimal(itemProperties[2])
+                        );
+
+                        // Add new InventoryItem to items list
+                        items.Add(item);
+                    } catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, ex.ToString());
+                    }
+                }
+            } catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.ToString());
+            }
 
             return items;
 		}
